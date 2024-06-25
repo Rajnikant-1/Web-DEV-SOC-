1a7cb0bdbb1241c8ceaefb4dbe83bf1cef12d574
Rajnikant <semardynastyrks16514@gmail.com>
2024-06-25 13:27:46 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,9 @@
+// function home(guest, age) {
+//     console.log('Welcome ' + guest +' gender - ' + age);
+// }
+
+// home('Akshay','25');
+function inHandSalary(CTC,bonus, tax, equity){
+    console.log(CTC - bonus - tax);
+} 
+inHandSalary(10000000,1500000,200000);
